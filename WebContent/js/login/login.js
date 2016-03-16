28f52c3ded7b1622a9bc2a385bec0f91e3e0206a
max <max@max-PC>
2016-03-15 23:10:40 -0430

    cambiando los js a un solo archivo

--- WebContent/js/login/login.js
+++ WebContent/js/login/login.js
@@ -1,5 +1,23 @@
-	var myApp = angular.module('myApp',['ngAnimate', 'ui.bootstrap']);
-	myApp.controller('BaseLoginController',function($scope,$uibModal,$rootScope){
+	var myApp = angular.module('myApp',['ngAnimate', 'ui.bootstrap'])
+	.directive('formElement', function() {
+		return {
+			restrict: 'E',
+			transclude: true,
+			scope: {
+				label : "@",
+				model : "="
+			},
+			link: function(scope, element, attrs) {
+				scope.disabled = attrs.hasOwnProperty('disabled');
+				scope.required = attrs.hasOwnProperty('required');
+				scope.pattern = attrs.pattern || '.*';
+			},
+			template: '<div class="form-group"><label class="col-sm-3 control-label no-padding-right" >'+
+			          ' {{label}}</label><div class="col-sm-7"><span class="block input-icon input-icon-right" '+
+			          'ng-transclude></span></div></div>'
+		};
+	})
+	.controller('BaseLoginController',function($scope,$uibModal,$rootScope){
 		$scope.isLoginUser = false;
 		$scope.loginWindow = function(){
 				var modalInstance = $uibModal.open({
@@ -18,9 +36,9 @@
 			$scope.loginUserName = username;
 		})
 	
-	});
+	})
 	
-	myApp.controller('LoginCtrl',function($scope,$uibModalInstance,$rootScope){
+	.controller('LoginCtrl',function($scope,$uibModalInstance,$rootScope){
 	$scope.user = {};
 	$scope.buttonText = "Login";
 	$scope.cancel = function () {
