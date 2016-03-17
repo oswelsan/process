7f1be195dc98a243b4c9cf9038a87440435d3fa6
max <max@max-PC>
2016-03-16 23:30:57 -0430

    modificando el login

--- WebContent/js/login/login.js
+++ WebContent/js/login/login.js
@@ -18,11 +18,12 @@
 		};
 	})
 	.controller('BaseLoginController',function($scope,$uibModal,$rootScope){
+		$scope.user = {};
 		$scope.isLoginUser = false;
 		$scope.loginWindow = function(){
 				var modalInstance = $uibModal.open({
 				      animation: true,
-				      templateUrl: 'pages/login/login.html',
+				      templateUrl: 'login.html',
 				      controller:'LoginCtrl',
 				      backdrop: "static",
 				    });
@@ -35,21 +36,11 @@
 			$scope.isLoginUser = true;
 			$scope.loginUserName = username;
 		})
+		
+		$scope.doLogin = function (user) {
+			$scope.buttonText = "Login...";
+			// Once the user is authenticated then a message to be broadcast to another controller.
+		    $rootScope.$broadcast('isLoginAction',user.username);
+		 }
 	
 	})
-	
-	.controller('LoginCtrl',function($scope,$uibModalInstance,$rootScope){
-	$scope.user = {};
-	$scope.buttonText = "Login";
-	$scope.cancel = function () {
-		$uibModalInstance.dismiss('cancel');
-	   
-	  };
-	
-	$scope.doLogin = function (user) {
-		$scope.buttonText = "Login...";
-		// Once the user is authenticated then a message to be broadcast to another controller.
-	    $rootScope.$broadcast('isLoginAction',user.username);
-	    $uibModalInstance.close();
-	 };
-	});
