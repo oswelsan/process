56c13eef983a5df94b89a9c3a464ad4ae08e1ab2
max <max@max-PC>
2016-03-30 21:11:35 -0430

    colocando el menu dinámico desde el controlador mainController

--- WebContent/app/controllers/mainController.js
+++ WebContent/app/controllers/mainController.js
@@ -12,43 +12,72 @@
 
         vm.title = "Now Viewing home!";
         vm.isCollapsed = true;
-        vm.services = [{
-            title: "Trabajo",
-            value: null,
+        vm.services = [
+        {
+            title: "TRABAJO",
+            value: "1",
             route: "root.tasks",
             subservices: [{
                 name: "Vencidos",
-                value: "2",
+                value: "11",
                 route: "root.tasks",
                 icon: "glyphicon glyphicon-alert text-danger",
                 subsubservices:[{
                 	name: "Vencidos 1",
-                    value: "2",
+                    value: "111",
                     route: "root.tasks",
                     icon: "glyphicon glyphicon-alert text-danger"
                 }, {
                 	name: "Vencidos 2",
-                    value: "2",
+                    value: "112",
                     route: "root.tasks",
                     icon: "glyphicon glyphicon-alert text-danger"
                 }]
             }, {
                 name: "En Riesgo",
-                value: "1",
+                value: "12",
                 route: "root.tasks",
                 icon: "glyphicon glyphicon-alert text-warning"
             }, {
                 name: "Vigentes",
-                value: "0",
+                value: "13",
                 route: "root.tasks",
                 icon: "glyphicon glyphicon-pencil text-primary"
-            }]
-        }];
-
+            }]      
+        },
+        {
+        	title: "CARTELERA",
+        	value: "2",
+        	route: "root.tasks",
+        	subservices: [{
+        	    name: "Alarmas",
+        	    value: "21",
+        	    route: "root.tasks",
+        	    icon: "glyphicon glyphicon-alert text-danger",
+        	    subsubservices:[{
+        	    	name: "Alarma 1",
+        	        value: "211",
+        	        route: "root.tasks",
+        	        icon: "glyphicon glyphicon-alert text-danger"
+        	    }, {
+        	    	name: "Alarma 2",
+        	        value: "212",
+        	        route: "root.tasks",
+        	        icon: "glyphicon glyphicon-alert text-danger"
+        	    }]
+        	}, {
+        	    name: "Administracion de ambiente",
+        	    value: "22",
+        	    route: "root.tasks",
+        	    icon: "glyphicon glyphicon-alert text-warning"
+        	}]      
+        }
+        
+        ];    
     }
-
-
 })();
+
+
 
 
 
